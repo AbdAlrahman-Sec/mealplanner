36999b6b367998ed6d09c0a87cb64cb9871c4f9e
Aabd2017 <112985057+Aabd2017@users.noreply.github.com>
2025-12-19 16:30:57 +0200

    Change database host and credentials
    Updated database connection details to use AWS.

--- api/get_recipes.php
+++ api/get_recipes.php
@@ -9,10 +9,10 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
 
 // Database configuration
-$host = 'localhost';
+$host = 'aws.connect.psdb.cloud';
 $dbname = 'mealplanner_db';
-$username = 'root';
-$password = '';
+$username = 'cngxyzjow7pojkrfrzvo';
+$password = 'pscale_pw_YcjYC8qcxYkNlDsCrx0CLdry9Y5elTXw9OzIbelW20R';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
